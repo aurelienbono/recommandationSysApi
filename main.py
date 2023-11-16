1246ae7b82a6749a1a2052b545642ac3d236a4cf
aurelienbono <bonombelleaurelien08@gmail.com>
2023-11-16 01:52:50 +0100

    add

--- main.py
+++ main.py
@@ -16,7 +16,3 @@ app = FastAPI(
 async def main():
     return {"message": "Hello World "}
 
-# Define a POST operation
-@app.post("/submit")
-async def submit(input: Input):
-    return {"message": f"Data submitted is: {input.text}"}
